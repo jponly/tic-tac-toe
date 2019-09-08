25d82d395e8e21e482fd195a17ee786afad62f56
jponly <jpangeli514@gmail.com>
2019-09-07 21:02:16 -0700

    updated game board

--- game/tictactoe.py
+++ game/tictactoe.py
@@ -1,2 +1,28 @@
-ms = "DOOKIE"
-print(ms)
+def display_board(board):
+
+    print(board[1]+'|'+board[2]+'|'+board[3])
+    print(board[4]+'|'+board[5]+'|'+board[6])
+    print(board[7]+'|'+board[8]+'|'+board[9])
+
+test_board = [' ']*10
+display_board(test_board)
+
+def player_input():
+    marker = ''
+
+    #keep asking player 1 to choose x or o
+
+    while not (marker != 'x' and marker != 'o'):
+        marker = input('Player 1, choose x or o: ').upper()
+
+    #assign player 2 the opposite marker
+    if marker == 'x':
+        return ('x','o')
+    else:
+            return('o','x')
+
+   
+
+
+
+ 
